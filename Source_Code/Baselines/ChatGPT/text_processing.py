95bded45e1f3556c01cd229d7e6b3434c063b1cd
contextcraft <waseemakram@ciitvehari.edu.pk>
2024-09-14 20:34:40 -0700

    Delete Source_Code/Baselines directory

--- Source_Code/Baselines/ChatGPT/text_processing.py
+++
@@ -1,9 +0,0 @@
-# text_processing.py
-
-class TextProcessing:
-    def __init__(self, chatgpt4_config):
-        self.chatgpt4_config = chatgpt4_config
-
-    def generate_output_for_prompt(self, prompt):
-        """Generates the response for a given prompt using ChatGPT-4."""
-        return self.chatgpt4_config.get_chatgpt4_response(prompt)
